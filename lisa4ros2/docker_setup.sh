9e5427da85c62a5ac8e032c11815846366b8cec8
Giacomo Zanatta <giacomo.zanatta@unive.it>
2024-03-13 20:36:04 +0100

    Fix
    Fix

--- lisa4ros2/docker_setup.sh
+++ lisa4ros2/docker_setup.sh
@@ -1,5 +1,4 @@
 ./gradlew clean
-python3 docker_setup_aux.py
 ./gradlew build -x test -x javadoc
 unzip -o build/distributions/lisa4ros2-0.1a1.zip -d build/distributions
 chmod 775 build/distributions/lisa4ros2-0.1a1/bin/lisa4ros2
--- lisa4ros2/docker_setup_aux.py
+++
@@ -1,6 +0,0 @@
-lines = ""
-with open("src/main/resources/templates/index.html", "r") as f:
-    lines = "".join(f.readlines())
-    lines = lines.replace("<div class=\"ml-4\"><h4><a href=\"/\">Home</a></h4></div>", "")
-with open("src/main/resources/templates/index.html", "w") as f:
-    f.write(lines)
